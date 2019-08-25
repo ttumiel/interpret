60df662cb3379a560f05d0e6ea939124052f7ab9
Thomas Tumiel <ttumiel@gmail.com>
2019-08-25 22:09:07 +0200

    add blur

--- src/transforms.py
+++ src/transforms.py
@@ -1,12 +1,27 @@
 from torchvision import transforms
+from torch import nn
 
-def get_transforms(size, mean, std, rotate=10, flip_hor=True, flip_vert=False, perspective=True, color_jitter=True, ):
+class Blur():
+    def __init__(self, kernel_size, variance=1., mean=1.):
+        channels=3
+        self.filter = nn.Conv2d(channels,channels,kernel_size, bias=False, groups=channels)
+        kernel = torch.ones((channels,1,kernel_size,kernel_size))/(kernel_size*kernel_size)
+
+        self.filter.weight.data = kernel
+        self.filter.weight.requires_grad_(False)
+
+    def __call__(self, x):
+        if x.dim() == 3:
+            x.unsqueeze_(0)
+        return self.filter(x).squeeze()
+
+def get_transforms(size, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], rotate=10, flip_hor=True, flip_vert=False, perspective=True, color_jitter=True, ):
     tfms = [
         transforms.Resize((size, size)),
         transforms.RandomRotation(rotate)
     ]
     if flip_hor: tfms += [transforms.RandomHorizontalFlip()]
-    if flip_vert tfms += [transforms.RandomVerticalFlip()]
+    if flip_vert: tfms += [transforms.RandomVerticalFlip()]
     if perspective: tfms += [transforms.RandomPerspective()]
 
     brightness, contrast, saturation, hue = 0.3, 0.2, 0.2, 0.2
