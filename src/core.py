90c6c4f4317514eb7cacf43a5bd9609f7563d804
Thomas Tumiel <ttumiel@gmail.com>
2019-08-28 23:25:19 +0200

    monkeypatch nn.Module.__getitem__

--- src/core.py
+++ src/core.py
@@ -1,1 +1,13 @@
-nn.Module.__getitem__ = lambda this,i: list(this.children())[i]
+"""Core utilities for this project"""
+
+def get(s,i):
+    if isinstance(i, int):
+        return list(s.children())[i]
+    elif isinstance(i, str):
+        layers = i.split("/")
+        l = s
+        for layer in layers:
+            l = getattr(l, layer)
+        return l
+
+nn.Module.__getitem__ = get
