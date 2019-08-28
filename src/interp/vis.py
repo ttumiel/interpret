930576a276b7327b74604426698d10447f9f237b
Thomas Tumiel <ttumiel@gmail.com>
2019-08-28 23:24:49 +0200

    visualisation methods

---
+++ src/interp/vis.py
@@ -0,0 +1,37 @@
+"Visualise models"
+
+from ..hooks import Hook
+import torch
+
+class CutModel():
+    "Class to visualise particular layers by optimisation"
+    def __init__(self, model, layer, channel):
+        self.model,self.layer,self.channel=model,layer,channel
+        self.active = False
+        for p in self.model.parameters():
+            p.requires_grad_(False)
+
+    def __call__(self, x):
+        def activation_fn(module,input,output):
+            self.loss = -torch.mean(output[:,self.channel])
+            self.active = True
+
+        with Hook(self.model[self.layer], activation_fn, detach=False):
+            for i,m in enumerate(self.model.children()):
+                x = m(x)
+                if self.active:
+                    self.active = False
+                    break
+        return x
+
+def random_im(size=64):
+    "Create a random 'image' that is normalized according to the network"
+    im = torch.rand((3,size,size))*30 + 160
+    mean = torch.tensor([0.485, 0.456, 0.406])
+    std = torch.tensor([0.229, 0.224, 0.225])
+    im /= 255
+    im -= mean[..., None, None]
+    im /= std[..., None, None]
+    im.unsqueeze_(0)
+    im.requires_grad_(True)
+    return im
