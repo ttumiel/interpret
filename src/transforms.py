07a4a76336555f94983267a783e9c112a97703ec
Thomas Tumiel <ttumiel@gmail.com>
2019-09-04 22:53:15 +0200

    add gaussian blur

--- src/transforms.py
+++ src/transforms.py
@@ -3,6 +3,7 @@ from torch import nn
 import torch
 import random, math
 from torch.nn.functional import affine_grid, grid_sample
+from torch.nn import functional as F
 
 class Blur():
     def __init__(self, kernel_size, variance=1., mean=1.):
@@ -19,13 +20,47 @@ class Blur():
             return self.filter(x).squeeze()
         return self.filter(x)
 
-class Rotate():
-    def __init__(self, max_angle):
-        self.angle = max_angle
+class GaussianBlur():
+    def __init__(self, channels, kernel_size, sigma, dim=2):
+        self.channels, self.kernel_size, self.sigma = channels, kernel_size, sigma
+        self.get_kernel(self.channels, self.kernel_size, self.sigma, dim=2)
+        self.conv = nn.Conv2d(channels, channels, kernel_size, groups=channels, bias=False, padding=kernel_size//2)
+        self.conv.weight.data = self.kernel
+        self.conv.weight.requires_grad_(False)
 
-    def __call__(self, t):
-        pass
+    def get_kernel(self, channels, kernel_size, sigma, dim=2):
+        kernel = 1
+        meshgrids = torch.meshgrid(
+            [
+                torch.arange(kernel_size, dtype=torch.float32)
+                for size in range(dim)
+            ]
+        )
+        for mgrid in meshgrids:
+            mean = (kernel_size - 1) / 2
+            kernel *= 1 / (sigma * math.sqrt(2 * math.pi)) * \
+                      torch.exp(-((mgrid - mean) / sigma) ** 2 / 2)
 
+        # Make sure sum of values in gaussian kernel equals 1.
+        kernel = kernel / torch.sum(kernel)
+
+        # Reshape to depthwise convolutional weight
+        kernel = kernel.view(1, 1, *kernel.size())
+        self.kernel = kernel.repeat(channels, *[1] * (kernel.dim() - 1))
+
+    def __call__(self, x):
+        if x.dim() == 3:
+            x.unsqueeze_(0)
+            return self.conv(x).squeeze()
+        return self.conv(x)
+
+class ReducingGaussianBlur(GaussianBlur):
+    def __call__(self, x):
+        self.sigma *= 0.95
+        self.get_kernel(self.channels, self.kernel_size, self.sigma)
+        self.conv.weight.data = self.kernel
+        self.conv.weight.requires_grad_(False)
+        return super().__call__(x)
 
 def get_transforms(size, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], rotate=10, flip_hor=True, flip_vert=False, perspective=True, color_jitter=True, ):
     tfms = [
@@ -50,7 +85,7 @@ def denorm(img):
 def affine(mat):
     "applies an affine transform"
     def inner(x):
-        if x.ndim == 3: x=x.unsqueeze(0)
+        if x.dim() == 3: x=x.unsqueeze(0)
         grid = affine_grid(mat, x.size())
         rot_im = grid_sample(x, grid, padding_mode="reflection")
         return rot_im
