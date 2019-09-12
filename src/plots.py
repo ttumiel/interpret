31e94c068d3ace4f510c4ff1442132e573c2069d
Thomas Tumiel <ttumiel@gmail.com>
2019-09-12 16:22:09 +0200

    plot method

--- src/plots.py
+++ src/plots.py
@@ -16,5 +16,17 @@ def show_images(batched_tensor, normalize=False, figsize=(5,5), axis=False, labe
     for i,ax in enumerate(axes):
         if i<batched_tensor.size(0):
             show_image(batched_tensor[i],normalize,ax)
+            if labels is not None: ax.set_title(f'{labels[i]}')
         if not axis: ax.set_axis_off()
-        if labels is not None: ax.set_title(f'{labels[i]}')
+
+def plot(y, x=None, title=None, ax=None, x_lb=None, y_lb=None):
+    if ax is None:
+        plt.plot(y) if x is None else plt.plot(y, x)
+        plt.title(title)
+        plt.xlabel(x_lb)
+        plt.ylabel(y_lb)
+    else:
+        ax.plot(y) if x is None else ax.plot(y, x)
+        ax.set_title(title)
+        ax.set_xlabel(x_lb)
+        ax.set_ylabel(y_lb)
