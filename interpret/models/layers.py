6949555fdbb4494ef2de506cd5223afdf727d96a
Thomas Tumiel <ttumiel@gmail.com>
2019-09-30 22:16:50 +0200

    architecture improvements

--- interpret/models/layers.py
+++ interpret/models/layers.py
@@ -9,17 +9,62 @@ from ..plots import plot
 def accuracy(y_hat, y):
     return (y_hat.argmax(-1) == y).float().mean().item()
 
-def init(m):
-    if isinstance(m, nn.Conv2d):
-        nn.init.kaiming_normal_(m.weight.data)
-        if hasattr(m, 'bias'):
-            m.bias.data.fill_(0.)
+def init_head(m):
+    if isinstance(m, (nn.BatchNorm2d, nn.BatchNorm1d, nn.BatchNorm3d)):
+        nn.init.constant_(m.weight.data, 1)
+        nn.init.constant_(m.bias.data, 0)
+    else:
+        init_default(m)
+
+def init_default(m, func=nn.init.kaiming_normal_):
+    "Initialize `m` weights with `func` and set `bias` to 0."
+    if func:
+        if hasattr(m, 'weight'): func(m.weight)
+        if hasattr(m, 'bias') and hasattr(m.bias, 'data'): m.bias.data.fill_(0.)
+    return m
 
 def conv(ni, nf, ks=3, strd=1, bn=False):
     layers = [nn.Conv2d(ni, nf, ks, padding=ks//2, stride=strd),
               nn.ReLU(True)]
     if bn: layers.append(nn.BatchNorm2d(nf))
     return nn.Sequential(*layers)
+
+def create_head(ni, nf, pool=True):
+    if pool:
+        layers = [
+            AdaptiveConcatPool2d(),
+            Flatten()
+        ]
+        ni = 2*ni
+    else:
+        layers = []
+
+    layers += [
+        nn.BatchNorm1d(ni),
+        nn.Dropout(p=0.25),
+        nn.Linear(ni, 512),
+        nn.ReLU(True),
+        nn.BatchNorm1d(512),
+        nn.Dropout(p=0.5),
+        nn.Linear(512, nf)
+    ]
+    return nn.Sequential(*layers).apply(init_head)
+
+def cut_arch(m, cut=-1):
+    body = OrderedDict(list(m.named_children())[:cut])
+    return nn.Sequential(body)
+
+class AdaptiveConcatPool2d(nn.Module):
+    "Layer that concats `AdaptiveAvgPool2d` and `AdaptiveMaxPool2d`."
+    def __init__(self, sz=None):
+        "Output will be 2*sz or 2 if sz is None"
+        super().__init__()
+        self.output_size = sz or 1
+        self.ap = nn.AdaptiveAvgPool2d(self.output_size)
+        self.mp = nn.AdaptiveMaxPool2d(self.output_size)
+
+    def forward(self, x):
+        return torch.cat([self.mp(x), self.ap(x)], 1)
 
 class Lambda(nn.Module):
     def __init__(self, fn):
