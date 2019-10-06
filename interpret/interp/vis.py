973884eb026db46408a916aa2e0f0b0141434f77
Thomas Tumiel <ttumiel@gmail.com>
2019-10-06 22:28:37 +0200

    overhaul OptVis class

--- interpret/interp/vis.py
+++ interpret/interp/vis.py
@@ -11,6 +11,7 @@ import torchvision
 from ..transforms import *
 import numpy as np
 from ..utils import denorm
+from .param import *
 
 VIS_TFMS = torchvision.transforms.Compose([
     RandomEvery([
@@ -53,31 +54,41 @@ class OptVis():
                     break
         return x
 
-    def vis(self, input_img, transform=False, iters=50, decorrelate=False, lr=0.05, wd=0.):
-        self.optim = self.optim_fn([input_img], lr=lr, weight_decay=wd)
-        for i in range(iters):
-            self.objective(input_img)
-            self.objective.loss.backward() # Maybe change to just objective.backward?
+    def vis(self, img_param, thresh=(500,), transform=True, lr=0.05, wd=0., verbose=True):
+        """
+        TODO: UPDATE
+        The `img_obj` object is a dictionary containing the item to optimise and the
+        item to display. The item to display must be a valid image which can be run
+        through the network. The item to optimise must be a leaf node in order to
+        be optimised.
+        """
+        self.optim = self.optim_fn(img_param.parameters(), lr=lr, weight_decay=wd)
+        for i in range(max(thresh)+1):
+            img = img_param()
 
-            if i % 100 == 0: # add if verbose/print_every
-                print(i, self.objective.loss.item())
-                display(zoom(denorm(input_img), 2))
+            if transform:
+                img = self.tfms(img)
+
+            self.objective(img)
+            self.objective.loss.backward()
+
+            # print(img_param.noise.grad.abs().max(), img_param.noise.grad.abs().mean(),img_param.noise.grad.std())
+
+            # Apply transforms to the gradient (normalize, blur, etc.)
+            # with torch.no_grad():
+            #     img_param.noise.grad.data = img_param.noise.grad.data / (img_param.noise.grad.data.std() + 1e-1)
+            #     input_img.grad.data = ReducingGaussianBlur(3, 3, 5)(input_img.grad.data)
+            # print(img_param.noise.grad.abs().max(), img_param.noise.grad.abs().mean(),img_param.noise.grad.std())
+
 
             self.optim.step()
             self.optim.zero_grad()
-            # input_img.data.clamp_(-2, 2)
 
-            if transform:
-                with torch.no_grad():
-                    input_img = self.tfms(input_img)
+            if verbose and i in thresh:
+                print(i, self.objective.loss.item())
+                display(zoom(denorm(img), 2))
 
-            # if decorrelate: # Move to image generation
-            #     input_img = _linear_decorelate_color(input_img).detach()
-
-            input_img = input_img.requires_grad_(True)
-            self.optim = self.optim_fn([input_img], lr=lr, weight_decay=wd)
-
-        return input_img
+            # self.optim.param_groups[0]['params'][0] = img_obj['optimise']
 
     @classmethod
     # layer and channel... How to make this extensible into layer,
@@ -153,46 +164,15 @@ class LayerObjective(Objective):
             msg += f"  {imagenet_labels[self.neuron]}"
         return msg
 
-def random_im(size=64):
-    "Create a random 'image' that is normalized according to the network"
-    im = torch.rand((3, size, size))*30 + 160
-    mean, std = imagenet_stats
-    mean, std = torch.tensor(mean), torch.tensor(std)
-    im /= 255
-    im -= mean[..., None, None]
-    im /= std[..., None, None]
-    im.unsqueeze_(0)
-    im.requires_grad_(True)
-    return im
+# class NeuronObjective(Objective):
+#     def __init__(self, model, layer):
+#         self.model = model
+#         self.layer = layer
 
-color_correlation_svd_sqrt = np.asarray([[0.26, 0.09, 0.02],
-                                        [0.27, 0.00, -0.05],
-                                        [0.27, -0.09, 0.03]]).astype("float32")
-
-
-max_norm_svd_sqrt = np.max(np.linalg.norm(color_correlation_svd_sqrt, axis=0))
-color_mean = [0.48, 0.46, 0.41]
-
-def _linear_decorelate_color(t):
-    """Multiply input by sqrt of empirical (ImageNet) color correlation matrix.
-
-    If you interpret t's innermost dimension as describing colors in a
-    decorrelated version of the color space (which is a very natural way to
-    describe colors -- see discussion in Feature Visualization article) the way
-    to map back to normal colors is multiply the square root of your color
-    correlations.
-    """
-    # check that inner dimension is 3?
-    t_flat = t.squeeze().view([3, -1])
-    color_correlation_normalized = color_correlation_svd_sqrt / max_norm_svd_sqrt
-    t_flat = torch.tensor(color_correlation_normalized).t() @ t_flat # should this be transposed??
-    t = t_flat.view(t.size())
-    return t
-
-def fourier_image(size=64, noise_scale=0.01, decorrelate=False):
-    noise=noise_scale*torch.randn([3,size,size,2])
-    tfm_noise = torch.fft(noise, 3, normalized=True)
-    noise = torch.irfft(tfm_noise, 3, onesided=False)
-    if decorrelate:
-        noise = _linear_decorelate_color(noise)
-    return noise.unsqueeze_(0).clone().detach().requires_grad_(True)
+#     def __call__(self, *inputs, **kwargs):
+#         if self.neuron is None:
+#             self.loss = -torch.mean(output[:, self.channel])
+#         else:
+#             if isinstance(module, nn.Conv2d):
+#                 self.loss = -torch.mean(output[:, self.channel, self.neuron])
+#         self.active = True
