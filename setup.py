c6adcf50b9269f29885fdb9407bc9c8aa9e85d4c
Thomas Tumiel <ttumiel@gmail.com>
2019-10-15 17:45:05 +0200

    add dependencies

--- setup.py
+++ setup.py
@@ -5,13 +5,22 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="interpret",
-    version="0.0.1",
+    version="0.0.2",
     author="Thomas Tumiel",
     description="Interpreting deep learning models in PyTorch.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ttumiel/deep-interp",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'numpy',
+          'matplotlib',
+          'pandas',
+          'Pillow',
+          'tqdm',
+          'torchvision',
+          'torch'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
