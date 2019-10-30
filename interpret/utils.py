e3be477db701b41bb3a8a7b600cb67f07a50966c
Thomas Tumiel <ttumiel@gmail.com>
2019-10-30 11:11:06 +0200

    typecheck norm()

--- interpret/utils.py
+++ interpret/utils.py
@@ -1,6 +1,7 @@
 import torch
 from pathlib import Path
 from PIL import Image
+import numpy as np
 
 from .imagenet import imagenet_stats
 
@@ -27,6 +28,14 @@ def denorm(im, mean=imagenet_stats[0], std=imagenet_stats[1], image=True):
 
 def norm(im, input_range=(0,255), mean=imagenet_stats[0], std=imagenet_stats[1], unsqueeze=True, grad=True):
     "Normalize an image"
+    if isinstance(im, Image.Image):
+        im = torch.tensor(np.asarray(im)).permute(2,0,1).float()
+    elif isinstance(im, np.array):
+        im = torch.tensor(im).float()
+        size = im.size()
+        assert len(size)==3 or len(size)==4, "Image has wrong number of dimensions."
+        assert size[0]==3 or size[0]==1, "Image has invalid channel number. Should be 1 or 3."
+
     mean, std = torch.tensor(mean, device=im.device), torch.tensor(std, device=im.device)
     im = im + input_range[0]
     im = im / input_range[1]
