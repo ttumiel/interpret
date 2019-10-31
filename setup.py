041168cc826331d06c3420b2afd2421898145bdf
Thomas Tumiel <ttumiel@gmail.com>
2019-10-31 14:25:56 +0200

    add build and test pipeline

--- setup.py
+++ setup.py
@@ -3,9 +3,17 @@ import setuptools
 with open("README.md", "r") as f:
     long_description = f.read()
 
+test_deps = [
+    'coverage',
+    'pytest',
+]
+extras = {
+    'test': test_deps,
+}
+
 setuptools.setup(
-    name="interpret",
-    version="0.0.2",
+    name="interpret-pytorch",
+    version="0.1.0",
     author="Thomas Tumiel",
     description="Interpreting deep learning models in PyTorch.",
     long_description=long_description,
@@ -13,17 +21,21 @@ setuptools.setup(
     url="https://github.com/ttumiel/deep-interp",
     packages=setuptools.find_packages(),
     install_requires=[
-          'numpy',
-          'matplotlib',
-          'pandas',
-          'Pillow',
-          'tqdm',
-          'torchvision',
-          'torch'
+        'numpy',
+        'matplotlib',
+        'pandas',
+        'Pillow',
+        'tqdm',
+        'torchvision',
+        'torch'
     ],
+    tests_require=test_deps,
+    extras_require=extras,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License"
     ],
     python_requires='>=3.6',
 )
