ca9b2c6c8a4430212d4333d33461ae578e80e054
Thomas Tumiel <ttumiel@gmail.com>
2020-05-25 21:13:27 +0200

    format vis test and add network fixture

--- test/test_vis.py
+++ test/test_vis.py
@@ -1,20 +1,37 @@
 import torchvision, torch, pytest
 
-from interpret import OptVis, ImageParam
-from interpret.utils import denorm
+from interpret import OptVis, ImageParam, ImageFile
 
-def test_neuron():
-    network = torchvision.models.vgg11(pretrained=False).to('cuda' if torch.cuda.is_available() else 'cpu')
-    layer = 'classifier/6'
-    neuron = 5
-    optvis = OptVis.from_layer(network, layer=layer, neuron=neuron)
-    img_param = ImageParam(224, fft=True, decorrelate=True)
-    optvis.vis(img_param, thresh=(10,), transform=True, lr=0.05, wd=0.9, verbose=False)
+@pytest.fixture
+def network():
+    return torchvision.models.vgg11(pretrained=False).to(
+        "cuda" if torch.cuda.is_available() else "cpu"
+    )
 
-def test_channel():
-    network = torchvision.models.vgg11(pretrained=False).to('cuda' if torch.cuda.is_available() else 'cpu')
-    layer = 'classifier/6'
-    channel = 23
-    optvis = OptVis.from_layer(network, layer=layer, channel=channel)
-    img_param = ImageParam(224, fft=True, decorrelate=True)
-    optvis.vis(img_param, thresh=(10,), transform=True, lr=0.05, wd=0.9, verbose=False)
+@pytest.fixture
+def imsize():
+    return 64
+
+def test_neuron(network, imsize):
+    optvis = OptVis.from_layer(network, layer="classifier/6", neuron=5)
+    img_param = ImageParam(imsize, fft=True, decorrelate=True)
+    optvis.vis(img_param, thresh=(10,), transform=True, lr=0.05, verbose=False)
+
+def test_channel(network, imsize):
+    optvis = OptVis.from_layer(network, layer="classifier/6", channel=23)
+
+    # with fft and decorrelate
+    img_param = ImageParam(imsize, fft=True, decorrelate=True)
+    optvis.vis(img_param, thresh=(10,), transform=True, lr=0.05, verbose=False)
+
+    # with fft and without decorrelate
+    img_param = ImageParam(imsize, fft=True, decorrelate=True)
+    optvis.vis(img_param, thresh=(10,), transform=True, lr=0.05, verbose=False)
+
+    # without fft and with decorrelate
+    img_param = ImageParam(imsize, fft=True, decorrelate=True)
+    optvis.vis(img_param, thresh=(10,), transform=True, lr=0.05, verbose=False)
+
+    # without fft and decorrelate
+    img_param = ImageParam(imsize, fft=False, decorrelate=False)
+    optvis.vis(img_param, thresh=(10,), transform=False, verbose=False)
