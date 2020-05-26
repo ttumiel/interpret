1fc77fcc74ce36adef1525579005bd4c3864ebc7
Thomas Tumiel <ttumiel@gmail.com>
2020-05-26 13:23:50 +0200

    assert loss decreases and shortcut works

--- test/test_vis.py
+++ test/test_vis.py
@@ -1,6 +1,8 @@
 import torchvision, torch, pytest
+import random
 
 from interpret import OptVis, ImageParam, ImageFile
+from interpret.hooks import Hook
 
 @pytest.fixture
 def network():
@@ -34,21 +36,35 @@ def test_channel(network, imsize, channel):
     optvis = OptVis.from_layer(network, layer="features/18", channel=channel, neuron=6)
     optvis.vis(thresh=(10,), verbose=False)
 
+def test_shortcut(network, imsize, channel):
+    def was_called(m,i,o):
+        raise Exception
+
+    with Hook(network[1], was_called):
+        optvis = OptVis.from_layer(network, layer="features/18", channel=channel, neuron=6, shortcut=True)
+        optvis.vis(thresh=(10,), verbose=False)
+
 def test_image_params(network, imsize, channel):
     optvis = OptVis.from_layer(network, layer="features/18", channel=channel, neuron=6)
+    optvis.vis(thresh=(1,))
+    init_loss = optvis.objective.loss
 
     # with fft and decorrelate
     img_param = ImageParam(imsize, fft=True, decorrelate=True)
     optvis.vis(img_param, thresh=(10,), verbose=False)
+    assert bool(optvis.objective.loss < init_loss)
 
     # with fft and without decorrelate
     img_param = ImageParam(imsize, fft=True, decorrelate=True)
     optvis.vis(img_param, thresh=(10,), verbose=False)
+    assert bool(optvis.objective.loss < init_loss)
 
     # without fft and with decorrelate
     img_param = ImageParam(imsize, fft=True, decorrelate=True)
     optvis.vis(img_param, thresh=(10,), verbose=False)
+    assert bool(optvis.objective.loss < init_loss)
 
     # without fft and decorrelate
     img_param = ImageParam(imsize, fft=False, decorrelate=False)
     optvis.vis(img_param, thresh=(10,), verbose=False)
+    assert bool(optvis.objective.loss < init_loss)
