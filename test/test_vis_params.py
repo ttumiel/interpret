89c0674ba33254234c67637083eeddc25aa30029
Thomas Tumiel <ttumiel@gmail.com>
2020-05-27 12:44:31 +0200

    add tests for visualisation objectives, params, integration. Cleanup files

---
+++ test/test_vis_params.py
@@ -0,0 +1,23 @@
+from interpret import OptVis, ImageParam, ImageFile
+
+from . import assert_loss_decreases
+
+
+def test_image_params(network, imsize, channel, n_steps):
+    optvis = OptVis.from_layer(network, layer="features/18", channel=channel, neuron=6)
+
+    # with fft and decorrelate
+    img_param = ImageParam(imsize, fft=True, decorrelate=True)
+    assert_loss_decreases(optvis, img_param=img_param, thresh=n_steps)
+
+    # with fft and without decorrelate
+    img_param = ImageParam(imsize, fft=True, decorrelate=True)
+    assert_loss_decreases(optvis, img_param=img_param, thresh=n_steps)
+
+    # without fft and with decorrelate
+    img_param = ImageParam(imsize, fft=True, decorrelate=True)
+    assert_loss_decreases(optvis, img_param=img_param, thresh=n_steps)
+
+    # without fft and decorrelate
+    img_param = ImageParam(imsize, fft=False, decorrelate=False)
+    assert_loss_decreases(optvis, img_param=img_param, thresh=n_steps)
