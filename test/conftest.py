8f159625d04e45b16eb37a7d8552e1e0180f6d5a
Thomas Tumiel <ttumiel@gmail.com>
2020-05-27 12:45:25 +0200

    add vis test fixtures

---
+++ test/conftest.py
@@ -0,0 +1,32 @@
+import torchvision, torch, pytest
+import random
+
+@pytest.fixture(scope='session')
+def network():
+    return torchvision.models.vgg11(pretrained=False).to(
+        "cuda" if torch.cuda.is_available() else "cpu"
+    )
+
+@pytest.fixture(scope='session')
+def layer():
+    return 'features/18'
+
+@pytest.fixture(scope='session')
+def imsize():
+    return 64
+
+@pytest.fixture(scope='session')
+def n_steps():
+    return (10,)
+
+@pytest.fixture
+def neuron():
+    return random.randrange(1000)
+
+@pytest.fixture
+def channel():
+    return random.randrange(512)
+
+@pytest.fixture
+def channels():
+    return (random.randrange(512), random.randrange(512))
