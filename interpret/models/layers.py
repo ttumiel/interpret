0d9a2607bffc51228bbd3e0173646a46da3759a1
Thomas Tumiel <ttumiel@gmail.com>
2020-06-08 17:59:18 +0200

    add generalized relu

--- interpret/models/layers.py
+++ interpret/models/layers.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 
-__all__ = ['Lambda']
+__all__ = ['Lambda', 'GeneralizedReLU']
 
 class Lambda(nn.Module):
     def __init__(self, fn):
@@ -11,3 +11,17 @@ class Lambda(nn.Module):
         return self.fn(x)
     def __repr__(self):
         return f"Lambda({self.fn.__name__})"
+
+
+class GeneralizedReLU(nn.Module):
+    def __init__(self, leak=None, bias=0, max_value=None, inplace=False):
+        super().__init__()
+        self.bias = bias
+        self.max_value = max_value
+        self.relu = nn.ReLU(inplace) if leak is None else nn.LeakyReLU(leak, inplace)
+
+    def forward(self, x):
+        x = self.relu(x) + self.bias
+        if self.max_value is not None:
+            x = torch.clamp_max_(x, self.max_value)
+        return x
