015a0244e81e35f695b894b2c1c8a1638c07a5b0
Thomas Tumiel <ttumiel@gmail.com>
2020-10-19 15:12:27 +0200

    add more plotting args and change from stepsize to number of steps.

--- interpret/misc/loss_landscape.py
+++ interpret/misc/loss_landscape.py
@@ -8,9 +8,8 @@ import matplotlib.pyplot as plt
 
 from interpret.misc import validate
 
-
-def loss_landscape(network, dataloader, dir1=None, dir2=None, dir1_bound=(-1,1), dir2_bound=(-1,1),
-                   proc_fn=np.log, clip=(-3,10), step=0.1, device=None):
+def loss_landscape(network, dataloader, loss_fn=nn.CrossEntropyLoss(), dir1=None, dir2=None,
+                   dir1_bound=(-1,1,20), dir2_bound=(-1,1,20), device=None):
     """Generate the losses of a network using a grid of weights,
     originating with the network's current weights, and moving
     in a grid pattern between (-dir1, dir1) and (-dir2, dir2).
@@ -19,9 +18,12 @@ def loss_landscape(network, dataloader, dir1=None, dir2=None, dir1_bound=(-1,1),
     the same keys as the network. For example, this can be set
     as the same network trained from a different random seed,
     a random initialization, or anything in between. Note that these
-    must be directions from the supplied network state dict
-    i.e the difference between the target state dict and the
-    network state dict.
+    are directions from the supplied `network` state dict
+    i.e the directions are added to the network's current state
+    dict.
+
+    For details, see: Li et al, Visualizing the Loss Landscape of
+    Neural Nets. https://arxiv.org/abs/1712.09913
 
     Parameters:
         network (nn.Module): The (trained) network to generate
@@ -30,16 +32,11 @@ def loss_landscape(network, dataloader, dir1=None, dir2=None, dir1_bound=(-1,1),
             the loss values from. Usually you can reduce the size of the
             dataset to ~5% of the original size without changing the
             landscape visualisation much.
+        loss_fn (callable): The loss function to evaluate.
         dir1 and dir2 (state_dict): the direction to move across the loss
             landscape. Defaults to random normal tensors.
-        dir1_bound and dir2_bound ((float, float)): the bounds to move in
-            the direction of dir1 and dir2. Usually unnecessary to change.
-        proc_fn (callable): function to post process the losses.
-            Use np.log to smooth large spikes in the landscape.
-            Use np.sqrt for less smoothing.
-            Use None to apply no post-processing.
-        clip (tuple): Clip the min and max loss values. Use None to ignore.
-        step (float): the fidelity of the loss landscape generation.
+        dir1_bound and dir2_bound ((float, float, int)): (start, stop, num_steps)
+            the bounds to move in the direction of dir1 and dir2.
         device (torch.device): torch device for validation.
 
     Returns (tuple):
@@ -49,36 +46,38 @@ def loss_landscape(network, dataloader, dir1=None, dir2=None, dir1_bound=(-1,1),
     trained_sd = copy.deepcopy(network.state_dict())
 
     losses = []
-    x_pts = math.ceil((dir1_bound[1]-dir1_bound[0])/step+1)
-    y_pts = math.ceil((dir2_bound[1]-dir2_bound[0])/step+1)
+    x_pts = dir1_bound[2]
+    y_pts = dir2_bound[2]
     total = x_pts*y_pts
-    for sd in tqdm(get_state_dicts(trained_sd, dir1, dir2, step=step,
+    for sd in tqdm(get_state_dicts(trained_sd, dir1, dir2,
                                    dir1_bound=dir1_bound, dir2_bound=dir2_bound),
                    total=total, desc='Generating losses'):
         network.load_state_dict(sd)
         p,y = validate(network, dataloader, device=device)
-        loss = nn.CrossEntropyLoss()(p,y.squeeze())
+        loss = loss_fn(p,y.squeeze())
         losses.append(loss.cpu().detach().numpy())
 
     # Restore original state
     network.load_state_dict(trained_sd)
 
-    X = np.arange(dir1_bound[0],dir1_bound[1]+step,step)
-    Y = np.arange(dir2_bound[0],dir2_bound[1]+step,step)
+    X = np.linspace(*dir1_bound)
+    Y = np.linspace(*dir2_bound)
     X,Y = np.meshgrid(X,Y)
     Z = np.array(losses).reshape((x_pts, y_pts)).T
 
     return X,Y,Z
 
-def plot_loss_landscape(XYZ=None, network=None, dl=None, mode='surface', elevation=40, angle=45,
-                       levels=None, figsize=(10,10), **pyplot_kwds):
+def plot_loss_landscape(XYZ=None, network=None, dl=None, mode='surface', proc_fn=np.log,
+                        clip=(-3,10), elevation=40, angle=45, levels=None, figsize=(10,10),
+                        label=False, cmap='viridis', **pyplot_kwds):
     """Plot the loss landscape as either a surface or contour plot.
     Use the outputs of `loss_landscape` directly or provide the network
-    to create the landscape from.
+    and dataloader to create the landscape from.
 
     Parameters:
         XYZ (tuple): The outputs of `loss_landscape`.
         network (nn.Module): Trained pytorch network to get the landscape of.
+        dl (DataLoader): Pytorch dataloader for landscape generation.
         mode (str): 'surface' or 'contour'
         proc_fn (callable): function to post process the losses.
             Use np.log to smooth large spikes in the landscape.
@@ -90,6 +89,8 @@ def plot_loss_landscape(XYZ=None, network=None, dl=None, mode='surface', elevati
         levels (list): the levels of the contour plot to show. Defaults to
             the min and max losses, with a step of 0.5.
         figsize (float, float): pyplot figure size.
+        label (bool): Add axis labels to the plot.
+        cmap: pyplot colourmap.
         **pyplot_kwds: kwargs passed on to surface or contour plot functions.
 
     Returns (Axes):
@@ -105,24 +106,28 @@ def plot_loss_landscape(XYZ=None, network=None, dl=None, mode='surface', elevati
     if mode == 'contour':
         levels = levels if levels is not None else np.arange(np.min(Z),np.max(Z),0.5)
         f,ax = plt.subplots(figsize=figsize)
-        c = ax.contour(X,Y,Z, levels=levels, **pyplot_kwds)
+        c = ax.contour(X,Y,Z, levels=levels, cmap=cmap, **pyplot_kwds)
         ax.clabel(c, inline=2, fontsize=8)
         f.colorbar(c)
     elif mode == 'surface':
         from mpl_toolkits.mplot3d import Axes3D
         fig = plt.figure(figsize=figsize)
         ax = fig.gca(projection='3d')
-        ax.plot_surface(X,Y,Z, rstride=1, cstride=1, cmap='viridis', edgecolor='none', **pyplot_kwds)
+        ax.plot_surface(X,Y,Z, rstride=1, cstride=1, cmap=cmap, edgecolor='none', **pyplot_kwds)
         ax.view_init(elevation, angle)
+        if label: ax.set_zlabel('Loss')
     else: raise ValueError('mode `{}` not recognised'.format(mode))
+    if label:
+        ax.set_xlabel('Dir 1')
+        ax.set_ylabel('Dir 2')
     return ax
 
-def get_state_dicts(original, dir1=None, dir2=None, step=0.1, dir1_bound=(-1,1), dir2_bound=(-1,1)):
+def get_state_dicts(original, dir1=None, dir2=None, dir1_bound=(-1,1,20), dir2_bound=(-1,1,20)):
     "Generate state dicts by interpolating the original with 2 directions."
     dir1 = dir1 or get_rand_dir(original)
     dir2 = dir2 or get_rand_dir(original)
-    for d1 in np.arange(dir1_bound[0], dir1_bound[1]+step, step):
-        for d2 in np.arange(dir2_bound[0], dir2_bound[1]+step, step):
+    for d1 in np.linspace(*dir1_bound):
+        for d2 in np.linspace(*dir2_bound):
             yield {k:v+dir1[k]*d1+dir2[k]*d2 for k,v in original.items()}
 
 def normalize_direction(d, w):
