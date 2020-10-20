baa5032f23be56e36fd21ba3a14518ba7fda45b1
Thomas Tumiel <ttumiel@gmail.com>
2020-10-20 15:51:33 +0200

    update misc tests

--- test/test_misc.py
+++ test/test_misc.py
@@ -1,10 +1,7 @@
 import torch
 from torch import nn
 
-from interpret import (top_losses, confusion_matrix,
-    plot_top_losses, plot_confusion_matrix,
-    get_dataset_examples, plot_dataset_examples,
-    loss_landscape, plot_loss_landscape)
+from interpret.misc import *
 
 class DeterministicNetwork(nn.Module):
     "A fake module that returns what you give it sequentially."
@@ -27,14 +24,8 @@ def test_top_losses(network, dataloader, conv_layer, n_classes, device):
     assert len(p) == len(y) == len(l) == len(i) == len(dataloader.dataset)
     assert torch.all(l.sort(descending=True).values == l)
 
-    p_,y_,l_,i_ = plot_top_losses(network, dataloader, loss_fn, n=n)
-    assert (p_==p).all() and (y_==y).all() and (l_==l).all() and (i_==i).all()
-
-    p_g,y_g,l_g,i_g = plot_top_losses(network, dataloader, loss_fn, n=n, gradcam=True, layer=conv_layer)
-    assert (p_==p_g).all() and (y_==y_g).all() and (l_==l_g).all() and (i_==i_g).all()
-
-    p_,y_,l_,i_ = plot_top_losses(network, dataloader, None, p,y,l,i)
-    assert (p_==p_g).all() and (y_==y_g).all() and (l_==l_g).all() and (i_==i_g).all()
+    plot_top_losses((p,y,l,i), dataloader)
+    plot_top_losses((p,y,l,i), dataloader, network=network, n=n, gradcam=True, layer=conv_layer)
 
     tgts = torch.cat([t for _,t in dataloader]).float()
     fake_net = DeterministicNetwork(tgts, device)
@@ -48,8 +39,7 @@ def test_confusion_matrix(network, dataloader, n_classes, device):
     assert cm.max() <= len(dataloader.dataset) and cm.min() >= 0
     assert cm.sum() == len(dataloader.dataset)
 
-    plot_confusion_matrix(n_classes, network=network, dataloader=dataloader)
-    plot_confusion_matrix(n_classes, cm=cm)
+    plot_confusion_matrix(cm)
 
     tgts = torch.cat([t for _,t in dataloader]).float()
     fake_net = DeterministicNetwork(tgts, device)
@@ -58,12 +48,11 @@ def test_confusion_matrix(network, dataloader, n_classes, device):
 
 
 def test_dataset_examples(network, dataloader, device, conv_layer, channel):
-    idxs = get_dataset_examples(network, dataloader, conv_layer, channel, device)
+    idxs = dataset_examples(network, dataloader, conv_layer, channel, device)
     assert len(idxs) == len(dataloader.dataset)
     assert idxs.min()>=0 and idxs.max()<len(dataloader.dataset)
 
-    plot_dataset_examples(2, dataloader, idxs=idxs)
-    plot_dataset_examples(2, dataloader, network=network, layer=conv_layer, channel=channel)
+    plot_dataset_examples(idxs, dataloader)
 
 
 def test_loss_landscape(network, dataloader, device):
